3dd39a53b4a7d28fc732f5003c631db38357da7d
Mike B. K <mikekristiansen88@gmail.com>
2023-05-16 09:46:10 +0200

    news feed styled 80% done. meetings done.

--- Scripts/newsFeed.js
+++ Scripts/newsFeed.js
@@ -1,25 +1,24 @@
 const newsFeed = {};
 
 newsFeed.TV2NORDtmpl = (item) =>
-  `<div>
-    <h1>${item.querySelector("title").textContent}</h1>
-    <p>
+  `<div class='feedCardContainer'>
+    <img class='feedCardImg' src="${item.querySelector("enclosure").getAttribute("url")}">
+    <div class='feedCardContent'>
+    <h1 class='feedTitle' >${item.querySelector("title").textContent}</h1>
+    <p class='feedDesc'>
         ${item.querySelector("description").textContent}<b>
     </p>
+    </div>
 </div>`;
 
 newsFeed.renderTV2NORD = (data) => {
-  const list = document.querySelector(".news-list");
+  const list = document.querySelector(".newsRss");
   const items = data.querySelectorAll("item");
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 4; i++) {
     console.log(); //Smid denne id i () for at se alle tags items[i]
     list.innerHTML += newsFeed.TV2NORDtmpl(items[i]);
   }
-
-  /*items.forEach((item) => {
-        list.innerHTML += newsFeed.TV2NORDtmpl(item);
-    });*/
 };
 
 newsFeed.init = () => {
